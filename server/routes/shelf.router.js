2b56d43755980b7640191f406033a281769e63d0
Tierra Byrd <tierrabyrd1@gmail.com>
2024-06-20 11:30:38 -0500

    post route added

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -2,18 +2,48 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+router.get('/', rejectUnauthenticated, (req, res) => {
+  const sqlText = `
+    SELECT * FROM item;
+  `;
+  pool.query(sqlText)
+  .then(dbRes => {
+    console.log('GET worked in /api/shelf!', dbRes.rows);
+    res.send(dbRes.rows);
+  })
+  .catch(dbErr => {
+    console.log('Error in /api/shelf ', dbErr);
+    res.sendStatus(500);
+  })
 });
 
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
   // endpoint functionality
+  const data = req.body.data;
+  const user = req.user;
+  const sqlText = `
+  INSERT INTO item
+  (description, image_url, user_id)
+  VALUES ($1, $2, $3);
+  `;
+  pool.query(sqlText, [data.description, data.image_url, user.id])
+  .then(response => {
+    console.log('POST worked in /api/shelf!');
+    res.sendStatus(201)
+  })
+  .catch(error => {
+    console.log('Error in /api/shelf ', error);
+    res.sendStatus(500);
+  })
 });
 
 /**
